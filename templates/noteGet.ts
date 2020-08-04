6f24b4819b3fd88b44f2b5425ae77e6a23d72a6d
Henrik Fricke <henrikfricke@web.de>
2020-08-04 11:43:31 +0200

    fix(get note): handle 404

--- templates/noteGet.ts
+++ templates/noteGet.ts
@@ -11,8 +11,16 @@ export const request = (tableName: string): string => `
 
 export const response = `
 #set($inputRoot = $input.path('$'))
+
+#if(!$inputRoot.toString().contains("Item"))
+#set($context.responseOverride.status = 404)
+{
+  "message":"Note not found"
+}
+#else
 {
   "id": "$inputRoot.Item.id.S",
   "text": "$inputRoot.Item.text.S"
 }
+#end
 `;
